c6ea39f1807b1675fc03e05ab75b014fc3e6b6e3
GUSTAVO RODRIGUES ARAUJO <ivan.paulino@vocacao.org.br>
2026-08-18 16:39:53 -0300

    update_modulo08

---
+++ modulo08_meudesenho_seunome.py
@@ -0,0 +1,50 @@
+'''
+Caveleiros do Zodiacos - Shiriu
+DBZ - Goku
+Thundercats - Lion
+Sailor Moon - Sailor Mars
+muppets baby - Gonzo
+'''
+
+
+class BonecoToyStory:
+
+    def __init__(self, nome, dono, frase_de_efeito):
+        self.nome = nome
+        self.dono = dono
+        self.frase_de_efeito = frase_de_efeito
+
+
+
+woody = BonecoToyStory(
+    nome = "Woody",
+    dono = "Andy",
+    frase_de_efeito = "Há uma cobra na minha bota!"
+)
+
+
+# betty
+
+
+
+# buzz
+
+
+
+# sr_potato
+
+
+
+#jessie
+
+
+
+# rex
+
+
+
+# slinky
+
+
+
+# fork
